3254f20df29e9b01d74824212b983cad41b3a07f
Simon Moreau <simon@bim42.com>
2021-01-18 14:55:55 +0100

    create a new function to build a mass

---
+++ EnvolopeByPolyline/README.md
@@ -0,0 +1,20 @@
+
+
+# Envolope by Polyline
+
+Generate a volume of a given width 
+
+|Input Name|Type|Description|
+|---|---|---|
+|Building Width|number|The width of the building|
+|Building Axis|https://hypar.io/Schemas/Geometry/Polyline.json|A planar continuous set of lines defining the general axis of the building|
+|Building Height|number|The height of the building|
+
+
+<br>
+
+|Output Name|Type|Description|
+|---|---|---|
+|Façade Area|Number|The surface of façade|
+|Roof Area|Number|The roof surface|
+
---
+++ EnvolopeByPolyline/src/EnvolopeByPolyline.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Elements;
+using Elements.Geometry;
+
+
+namespace EnvolopeByPolyline
+{
+    public static class EnvolopeByPolyline
+    {
+        /// <summary>
+        /// Generate a volume of a given width 
+        /// </summary>
+        /// <param name="model">The input model.</param>
+        /// <param name="input">The arguments to the execution.</param>
+        /// <returns>A EnvolopeByPolylineOutputs instance containing computed results and the model with any new elements.</returns>
+        public static EnvolopeByPolylineOutputs Execute(Dictionary<string, Model> inputModels, EnvolopeByPolylineInputs input)
+        {
+            Polyline polyline = input.BuildingAxis;
+
+            Polygon perimeter = input.BuildingAxis.Offset(input.BuildingWidth / 2, EndType.Butt).First();
+
+            var envMatl = new Material("envelope", new Color(0.3, 0.7, 0.7, 0.6), 0.0f, 0.0f);
+            var envelopes = new List<Envelope>();
+
+            // Create the Envelope at the location's zero plane.
+            var output = new EnvolopeByPolylineOutputs(perimeter.ToPolyline().Length() * input.BuildingHeight, perimeter.Area());
+
+            var extrude = new Elements.Geometry.Solids.Extrude(perimeter, input.BuildingHeight, Vector3.ZAxis, false);
+            var geomRep = new Representation(new List<Elements.Geometry.Solids.SolidOperation>() { extrude });
+            envelopes.Add(new Envelope(perimeter, 0.0, input.BuildingHeight, Vector3.ZAxis, 0.0,
+                          new Transform(), envMatl, geomRep, false, Guid.NewGuid(), ""));
+            output.Model.AddElements(envelopes);
+
+            var sketch = new ModelCurve(polyline, name: "Centerline Sketch");
+            output.Model.AddElement(sketch);
+            return output;
+        }
+    }
+}
